5d3c3da96211ea9ee86c0b4d3f26f970ec44e29f
Solomon Zitter <solomonzitter@gmail.com>
2025-12-15 10:56:50 -0800

    Additional formatting fixes

--- keyboards/boardsource/unicorne/keymaps/szitter/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/szitter/keymap.c
@@ -17,35 +17,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         C(G(KC_Q)),     KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,               KC_O,               KC_P,               KC_BSLS,
         _______,        LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,                                           KC_H,           RSFT_T(KC_J),   RGUI_T(KC_K),       RALT_T(KC_L),       RCTL_T(KC_SCLN),    KC_QUOT,
         QK_BOOT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMM,            KC_DOT,             KC_SLSH,            _______,
-                                                                        LT(_NUM, KC_TAB),LT(_NAV, KC_SPC),LT(_MSE, KC_NO),                              A(KC_BSPC),     KC_NO,          LT(_SYM, KC_DELETE)
+                                                        LT(_NUM, KC_TAB),LT(_NAV, KC_SPC),LT(_MSE, KC_NO),                              A(KC_BSPC),     KC_NO,          LT(_SYM, KC_DELETE)
     ),
 
     [_NUM] = LAYOUT_split_3x6_3(
         _______,        _______,        _______,        _______,        _______,        _______,                                        KC_PLUS,        KC_7,           KC_8,               KC_9,               KC_HASH,            KC_NO,
         _______,        _______,        _______,        _______,        _______,        _______,                                        KC_EQUAL,       KC_4,           KC_5,               KC_6,               KC_PERC,            KC_NO,
         _______,        _______,        _______,        _______,        _______,        _______,                                        KC_MINUS,       KC_1,           KC_2,               KC_3,               KC_KP_COMMA,        _______,
-                                                                _______,                _______,    _______,                _______,                KC_DOT,                 KC_0
+                                                        _______,        _______,        _______,                                        _______,        KC_DOT,         KC_0
     ),
 
     [_SYM] = LAYOUT_split_3x6_3(
         _______,        KC_KP_ASTERISK, KC_AT,          KC_LCBR,        KC_RCBR,        KC_CIRC,                                        _______,        _______,        _______,            _______,            _______,            _______,
         _______,        KC_EXLM,        KC_GRAVE,       KC_LPRN,        KC_RPRN,        KC_DLR,                                         _______,        _______,        _______,            _______,            _______,            _______,
         _______,        KC_AMPR,        KC_PIPE,        KC_LBRC,        KC_RBRC,        KC_TRANSPARENT,                                 _______,        _______,        _______,            _______,            _______,            _______,
-                                                                    KC_UNDS,                    _______,                        _______,            _______,            _______,                 _______
+                                                        KC_UNDS,        _______,        _______,                                        _______,        _______,        _______
     ),
 
     [_NAV] = LAYOUT_split_3x6_3(
         _______,        _______,        _______,        _______,        _______,        _______,                                        A(KC_LEFT),     G(A(KC_LEFT)),  G(A(KC_RIGHT)),     A(KC_RIGHT),        _______,            _______,
         _______,        _______,        _______,        _______,        _______,        _______,                                        KC_LEFT,        KC_DOWN,        KC_UP,              KC_RIGHT,           _______,            _______,
         _______,        _______,        _______,        _______,        _______,        _______,                                        G(KC_LEFT),     G(C(KC_LEFT)),  G(C(KC_RIGHT)),     G(KC_RIGHT),        _______,            _______,
-                                                                    _______,                    _______,                        _______,            _______,            _______,                 _______
+                                                        _______,        _______,        _______,                                        _______,        _______,        _______
     ),
 
     [_MSE] = LAYOUT_split_3x6_3(
         _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        _______,        MS_UP,              _______,            _______,            _______,
         _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        MS_LEFT,        MS_DOWN,            MS_RGHT,            _______,            _______,
         _______,        _______,        _______,        _______,        _______,        _______,                                        MS_WHLL,        MS_WHLD,        MS_WHLU,            MS_WHLR,            _______,            _______,
-                                                                    _______,                    _______,                        _______,            _______,            MS_BTN1,                 MS_BTN2
+                                                        _______,        _______,        _______,                                        _______,        MS_BTN1,        MS_BTN2
     ),
 
 };
