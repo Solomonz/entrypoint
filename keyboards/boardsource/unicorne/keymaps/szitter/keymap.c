1a3b13744b34ac0e8c930e66db164f21ade23141
Solomon Zitter <solomonzitter@gmail.com>
2025-12-15 10:58:11 -0800

    Downgrade to normal backspace instead of by word

--- keyboards/boardsource/unicorne/keymaps/szitter/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/szitter/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         C(G(KC_Q)),     KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,               KC_O,               KC_P,               KC_BSLS,
         _______,        LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,                                           KC_H,           RSFT_T(KC_J),   RGUI_T(KC_K),       RALT_T(KC_L),       RCTL_T(KC_SCLN),    KC_QUOT,
         QK_BOOT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMM,            KC_DOT,             KC_SLSH,            _______,
-                                                        LT(_NUM, KC_TAB),LT(_NAV, KC_SPC),LT(_MSE, KC_NO),                              A(KC_BSPC),     KC_NO,          LT(_SYM, KC_DELETE)
+                                                        LT(_NUM, KC_TAB),LT(_NAV, KC_SPC),LT(_MSE, KC_NO),                              KC_BSPC,        KC_NO,          LT(_SYM, KC_DELETE)
     ),
 
     [_NUM] = LAYOUT_split_3x6_3(
@@ -96,12 +96,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (is_in_command_tab) {
                 tap_code(KC_ESC);
                 is_in_command_tab = false;
-                return false;
-            }
-            break;
-        case A(KC_BSPC):
-            if (record->event.pressed && (get_mods() & MOD_MASK_SHIFT)) {
-                tap_code(KC_BSPC);
                 return false;
             }
             break;
