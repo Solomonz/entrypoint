51826d228099e1bf38a73194e4ea052ba690c7a0
Solomon Zitter <solomonzitter@gmail.com>
2025-12-28 11:48:10 -0800

    Basic OLED logic
    Show active layer on primary and QMK logo on secondary

--- keyboards/boardsource/unicorne/keymaps/szitter/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/szitter/keymap.c
@@ -85,6 +85,7 @@ const uint16_t PROGMEM graphite_escape_combo[]    = {KC_H, RGUI_T(KC_A), COMBO_E
 const uint16_t PROGMEM graphite_enter_combo[]     = {LSFT_T(KC_S), LGUI_T(KC_T), COMBO_END};
 const uint16_t PROGMEM graphite_caps_word_combo[] = {LSFT_T(KC_S), KC_H, COMBO_END};
 
+// clang-format off
 combo_t key_combos[] = {
     [BASE_ESCAPE_COMBO] = COMBO(base_escape_combo, KC_ESCAPE),
     [BASE_ENTER_COMBO] = COMBO(base_enter_combo, KC_ENTER),
@@ -93,7 +94,7 @@ combo_t key_combos[] = {
     [GRAPHITE_ENTER_COMBO] = COMBO(graphite_enter_combo, KC_ENTER),
     [GRAPHITE_CAPS_WORD_COMBO] = COMBO(graphite_caps_word_combo, QK_CAPS_WORD_TOGGLE),
 };
-
+// clang-format on
 
 bool get_combo_must_tap(uint16_t combo_index, combo_t *combo) {
     return combo_index == BASE_ENTER_COMBO || combo_index == GRAPHITE_ENTER_COMBO;
@@ -240,6 +241,61 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 /*******************/
 /* RGB SECTION END */
 /*******************/
+
+/**********************/
+/* OLED SECTION BEGIN */
+/**********************/
+
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_left()) {
+        return OLED_ROTATION_270; // The enemy's gate is down
+    } else {
+        return rotation;
+    }
+}
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00};
+
+    oled_write_P(qmk_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_left()) {
+        oled_write_P(PSTR("  "), false);
+        switch (get_highest_layer(layer_state)) {
+            case _BSE:
+                oled_write_ln_P(PSTR("BSE"), false);
+                break;
+            case _QWR:
+                oled_write_ln_P(PSTR("QWR"), false);
+                break;
+            case _NUM:
+                oled_write_ln_P(PSTR("NUM"), false);
+                break;
+            case _SYM:
+                oled_write_ln_P(PSTR("SYM"), false);
+                break;
+            case _NAV:
+                oled_write_ln_P(PSTR("NAV"), false);
+                break;
+            default:
+                oled_write_ln_P(PSTR("Undefined"), false);
+                break;
+        }
+    } else {
+        render_logo();
+    }
+
+    return false;
+}
+#endif
+
+/********************/
+/* OLED SECTION END */
+/********************/
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
